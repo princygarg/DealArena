cb41af43cbabec04a627b908eacba8fcaca4f040
Geet Agarwal <55127796+Geet99@users.noreply.github.com>
2020-06-18 03:09:15 +0530

    Update Routes.js

--- src/Routes.js
+++ src/Routes.js
@@ -4,6 +4,7 @@ import { Router, Switch, Route } from "react-router-dom";
 
 import i1login from "./components/i1login";
 import i2login from "./components/i2login";
+import i1form from "./components/i1form";
 import Home from "./components/Home";
 import userhome from "./components/userhome";
 
@@ -18,6 +19,7 @@ export default class Routes extends Component {
                     <Route path="/" exact component={Home} />
                     <Route path="/i1login" component={i1login} />
                     <Route path="/i2login" component={i2login} />
+                    <Route path="/i1form" component={i1form} />
                     <Route path="/userhome" component={userhome}/>
                 </Switch>
             </Router>
