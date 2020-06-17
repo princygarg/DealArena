fd70d662239131b19b1b2ded8c78018850d10037
Geet Agarwal <55127796+Geet99@users.noreply.github.com>
2020-06-18 02:20:24 +0530

    Add files via upload

---
+++ src/components/i2login.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { MDBContainer, MDBRow, MDBCol, MDBCard, MDBCardBody, MDBInput, MDBBtn } from 'mdbreact';
+
+class i2login extends Component{
+    render(){
+        return(
+            <MDBContainer>
+              <MDBRow>
+                <MDBCol md="6">
+                  <MDBCard>
+                    <div className="header pt-3 peach-gradient">
+                      <MDBRow className="d-flex justify-content-center">
+                        <h3 className="white-text mb-3 pt-3 font-weight-bold"> Sign Up </h3>
+                      </MDBRow>
+                    </div>
+
+                    <MDBCardBody className="mx-4 mt-4">
+                      <MDBInput label="Your name" group type="text" validate />
+                      <MDBInput label="Your email" group type="text" validate />
+                      <MDBInput label="Your company/brand name" group type="text" validate />
+                      <MDBInput label="Your password" group type="password" validate />
+
+                      <div className="text-center">
+                        <MDBBtn color="grey" rounded type="button" className="z-depth-1a" > Sign Up </MDBBtn>
+                      </div>
+                    </MDBCardBody>
+                  </MDBCard>
+                </MDBCol>
+              </MDBRow>
+            </MDBContainer>
+        );
+    }
+}
+
+export default i2login;
